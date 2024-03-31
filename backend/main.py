90c8b1a68b9b4c920d4facd00d11a8a301a6b191
Fedor <fedor1belov@gmail.com>
2024-03-31 11:17:11 +0300

    work?

--- backend/main.py
+++ backend/main.py
@@ -6,4 +6,4 @@ url = "https://olimp.miet.ru/ppo_it_final"
 data = requests.get(url, headers={"X-Auth-Token": "ppo_11_19825"}, params={"day": 25, "month": 1, "year": 23})
 print()
 
-print(*data.json())
+print(data.json())
