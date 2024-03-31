e3f766409d7e88ccb496406d49831196a2808356
G1ebs <glev.shooter06@gmail.com>
2024-03-31 11:08:52 +0300

    test getted api

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+import requests
+
+
+url = "https://olimp.miet.ru/ppo_it_final"
+
+data = requests.get(url, headers={"X-Auth-Token": "ppo_11_19825"}, params={"day": 25, "month": 1, "year": 23})
+print()
+
+print(*data.json())
